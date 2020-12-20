f0c8d84be53f23b1fd62343857d6178f41fb34a8
Anwesh43 <anweshthecool0@gmail.com>
2020-12-21 02:53:43 +0530

    adding PINode class

--- pieincreasingview/src/main/java/com/example/pieincreasingview/PieIncreasingView.kt
+++ pieincreasingview/src/main/java/com/example/pieincreasingview/PieIncreasingView.kt
@@ -122,4 +122,45 @@ class PieIncreasingView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PINode(var i : Int, val state : State = State()) {
+
+        private var next : PINode? = null
+        private var prev : PINode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PINode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPINode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PINode {
+            var curr : PINode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
