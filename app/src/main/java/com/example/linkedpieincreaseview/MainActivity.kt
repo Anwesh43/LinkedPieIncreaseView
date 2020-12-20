d3479197af81bd0902b3065d8eab305b400430e6
Anwesh43 <anweshthecool0@gmail.com>
2020-12-21 03:06:49 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedpieincreaseview/MainActivity.kt
+++ app/src/main/java/com/example/linkedpieincreaseview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedpieincreaseview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.pieincreasingview.PieIncreasingView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PieIncreasingView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
